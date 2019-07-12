b2ef3804d81f8820c55163c04f5463d8c0c28162
nagapremika <sadhupremika@gmail.com>
2019-07-12 08:00:42 +0530

    Commiting task3

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -23,18 +23,11 @@ public class Main {
 
 //       Dependency injection using ApplicationContext
         ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        Movie movie1=context.getBean("movie1",Movie.class);     //Injecting movie1
+      Movie movie1=context.getBean("movie1",Movie.class);     //Injecting movie1 by using autowire=byName
         movie1.display();
-        Movie movie2=context.getBean("movie2",Movie.class);        //Injecting movie2
+       Movie movie2=context.getBean("movie2",Movie.class);        //Injecting movie2 using constructor injection
         movie2.display();
-        Movie movie3=context.getBean("movie3",Movie.class);         //Injecting movie3
-        movie3.display();
-        System.out.println(movie2==movie3);               //  Comparing movie2 and movie3
-        Movie movie4=context.getBean("movie4",Movie.class);     //Injecting movie4
-        movie4.display();
-        Movie movie5=context.getBean("movie5",Movie.class);     //Injecting movie5
-        movie5.display();
-        System.out.println(movie4==movie5);
+
 
 
 
--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -1,11 +1,13 @@
 package com.stackroute.domain;
 
 public class Movie {
-   
-    public Movie(Actor actor) {
+    public Movie() {
+    }
+
+    public void setActor(Actor actor) {
         this.actor = actor;
     }
- public void setActor(Actor actor) {
+    public Movie(Actor actor) {
         this.actor = actor;
     }
 
