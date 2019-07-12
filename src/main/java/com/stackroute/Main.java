ba49184cee022daf8be80ab9cf8e2f2e8f934c35
nagapremika <51943079+nagapremika@users.noreply.github.com>
2019-07-12 10:09:07 +0530

    Update Main.java

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -2,21 +2,9 @@ package com.stackroute;
 
 
 import com.stackroute.domain.Movie;
-import org.springframework.beans.factory.BeanFactory;
 
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-
-import org.springframework.context.support.GenericApplicationContext;
-import org.springframework.core.io.ClassPathResource;
-
-
 
 public class Main {
     public static void main(String[] args) {
